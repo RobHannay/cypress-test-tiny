93be9c5a441dab3a2ae7faab009cffe3b553ef9c
Rob Hannay <rob@robhannay.co.uk>
2023-06-27 13:11:04 +0000

    Add fixture and file input html

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,6 +1,11 @@
 /// <reference types="cypress" />
 describe('page', () => {
   it('works', () => {
-    cy.visit('https://example.cypress.io')
+    cy.visit('https://example.cypress.io');
+
+    cy.get('input[type=file]')
+      .first()
+      .selectFile('cypress/fixtures/videos/Koala1.mp4', { force: true }, { action: 'drag-drop' });
+
   })
 })
