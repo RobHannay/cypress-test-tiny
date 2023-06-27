d314d59a0c37832d28f6ae4e80566b3ae8c55ab3
Rob Hannay <rob@robhannay.co.uk>
2023-06-27 13:23:22 +0000

    Point at local html file

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,7 +1,7 @@
 /// <reference types="cypress" />
 describe('page', () => {
   it('works', () => {
-    cy.visit('https://example.cypress.io');
+    cy.visit('./app/files.html');
 
     cy.get('input[type=file]')
       .first()
